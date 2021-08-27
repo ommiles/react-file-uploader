ec05076fc7850f3d029aeda1ec9056a0612c4c2b
OM Miles <miles@ommiles.com>
2021-08-27 00:06:22 -0700

    update cors

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const fileUpload = require('express-fileupload');
-import Cors from 'cors';
+const Cors = require('cors');
 
 const app = express(); // initialize express
 app.use(fileUpload()); // initialize express-fileupload
-app.use(Cors()); // 
+app.use(Cors());
 
 // we just need one endpoint for our upload
 // endpoint we want to send req to from react
