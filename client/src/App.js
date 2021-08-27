17d4a981e7404b3b4bc62f75356bba21f98d44a3
OM Miles <miles@ommiles.com>
2021-08-26 21:45:47 -0700

    add font awesome and start fileupload.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,20 @@
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faReact } from '@fortawesome/free-solid-svg-icons';
+// import { faReact } from '@fortawesome/fontawesome-svg-core';
+// import { faReact } from '@fortawesome/react-fontawesome';
+import { faReact } from '@fortawesome/free-brands-svg-icons';
+import FileUpload from './components/FileUpload';
 
 function App() {
-  return <div className='App'>App</div>;
+  return (
+    <div className='App'>
+      <h4>
+        <FontAwesomeIcon icon={faReact} />
+        React File Upload
+      </h4>
+    </div>
+  );
 }
 
 export default App;
